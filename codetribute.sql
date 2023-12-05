3c405ea8869de51afba8b5e5fb9b80e82993e874
Muhammad Ahmed <60579788+ahmed-develops@users.noreply.github.com>
2023-12-05 17:19:20 +0500

    Update codetribute.sql

--- codetribute.sql
+++ codetribute.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2023 at 03:49 PM
--- Server version: 10.4.28-MariaDB-log
+-- Generation Time: Dec 05, 2023 at 01:19 PM
+-- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `codetribute`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdateUser` (IN `user_id` VARCHAR(255), IN `name` VARCHAR(255), IN `email` VARCHAR(255), IN `password` VARCHAR(255), IN `phone_number` VARCHAR(255))   BEGIN
+  SET @sql_text = CONCAT('UPDATE users SET name = ?, email = ?, password = ?, phone_number = ? WHERE user_id = ?');
+
+  PREPARE stmt FROM @sql_text;
+
+  EXECUTE stmt USING name, email, password, phone_number, user_id;
+
+  DEALLOCATE PREPARE stmt;
+  
+  INSERT INTO transaction_log (actor_id, operation_type, table_name, query)
+  VALUES (user_id, 'UPDATE', 'users', @sql_text);
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -75,11 +94,30 @@ CREATE TABLE `payments` (
 --
 
 INSERT INTO `payments` (`transaction_id`, `sender_user_id`, `receiver_user_id`, `amount`, `timestamp`) VALUES
-('0X3F23FI2FMI4FMN', 'C01', 'P06', 2000, '2023-11-19 10:13:02'),
-('0xefef234ioiojfei', 'P25', 'C01', 1000, '2023-11-19 10:02:00'),
-('1fc1b610cd35e5fdf3fcd76b528521b2c962d93b', 'C01', 'P25', 20000, '2023-11-19 20:00:50'),
-('9ec187dbdca1dabdf7ca3a8a0fc8c25790be0c96', 'C01', 'P25', 2000, '2023-11-19 19:58:04'),
-('ff2b8e5e0004c260929f28dcc81c3e4c932c9bf0', 'C01', 'P25', 200, '0000-00-00 00:00:00');
+('4d6e0b140bc3e81e1cd95f51c4989f3a4a742eb4', 'A01', 'P25', 499, '2023-12-05 16:46:28'),
+('56e055f2a2103a625ab444f80b942e33ef61462f', 'P25', 'C01', 12000, '2023-12-05 15:50:16'),
+('ec9925fa46e5a956a140626d97b312c47befd2f9', 'C01', 'P25', 2000, '2023-12-05 15:48:12');
+
+--
+-- Triggers `payments`
+--
+DELIMITER $$
+CREATE TRIGGER `paymentTrigger` AFTER INSERT ON `payments` FOR EACH ROW BEGIN
+  -- Declare a variable to store the SQL statement
+  DECLARE sql_text LONGTEXT;
+
+  -- Get the full SQL statement from information_schema
+  SELECT CONCAT('INSERT INTO payments (sender_user_id, receiver_user_id, amount) VALUES (', NEW.sender_user_id, ', ', NEW.receiver_user_id, ', ', NEW.amount, ');')
+  INTO sql_text
+  FROM information_schema.processlist
+  WHERE id = CONNECTION_ID();
+
+  -- Insert data from the NEW row into transaction_log
+  INSERT INTO transaction_log (actor_id, operation_type, table_name, query)
+  VALUES (NEW.sender_user_id, 'INSERT', 'payments', sql_text);
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -95,7 +133,7 @@ CREATE TABLE `projectbase` (
   `code_path` varchar(500) NOT NULL,
   `tokens_offered` int(11) NOT NULL DEFAULT 0,
   `tokens_required` int(11) NOT NULL DEFAULT 0
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `projectbase`
@@ -118,9 +156,9 @@ INSERT INTO `projectbase` (`project_id`, `project_name`, `project_description`, 
 CREATE TABLE `transaction_log` (
   `log_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` varchar(3) DEFAULT NULL,
-  `table_name` varchar(50) DEFAULT NULL,
+  `actor_id` varchar(3) DEFAULT NULL,
   `operation_type` varchar(50) DEFAULT NULL,
+  `table_name` varchar(50) DEFAULT NULL,
   `query` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -128,22 +166,11 @@ CREATE TABLE `transaction_log` (
 -- Dumping data for table `transaction_log`
 --
 
-INSERT INTO `transaction_log` (`log_id`, `timestamp`, `user_id`, `table_name`, `operation_type`, `query`) VALUES
-(5, '2023-11-25 15:29:45', 'C69', 'users', 'INSERT', 'INSERT INTO users (user_id, name, email, password, phone_number, privilege VALUES (?,?,?,?,?,?)'),
-(6, '2023-11-25 15:59:00', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shahee, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680, privilege=contributor WHERE user_id=C01'),
-(7, '2023-11-25 16:00:10', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shaheer, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680, privilege=contributor WHERE user_id=C01'),
-(9, '2023-11-25 16:37:15', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shah, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680, privilege=contributor WHERE user_id=C01'),
-(10, '2023-11-25 16:45:09', 'A01', 'users', 'INSERT', 'INSERT INTO users (user_id, name, email, password, phone_number, privilege VALUES (C78,Rajesh,rajesh@rajesh.com,password,21434234234,Contributor)'),
-(11, '2023-11-25 16:45:40', 'A01', 'users', 'INSERT', 'INSERT INTO users (user_id, name, email, password, phone_number, privilege VALUES (P99,Khanna,khanna@khanna.com,johnny,2412343215,Publisher)'),
-(12, '2023-11-25 16:51:24', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shaheer, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680 WHERE user_id=C01'),
-(14, '2023-11-25 16:56:47', 'A01', 'users', 'INSERT', 'INSERT INTO users (user_id, name, email, password, phone_number, privilege VALUES (C85,Hannah,hannah@hannah.com,imgood,34425245,Contributor)'),
-(15, '2023-11-25 16:58:03', 'A01', 'users', 'INSERT', 'INSERT INTO users (user_id, name, email, password, phone_number, privilege VALUES (P90,Graves,graves@graves.com,graves,24235346534,Publisher)'),
-(16, '2023-11-25 16:59:23', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shahee, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680 WHERE user_id=C01'),
-(17, '2023-11-25 17:01:21', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shaheer, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680 WHERE user_id=C01'),
-(18, '2023-11-25 17:01:47', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shah, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680 WHERE user_id=C01'),
-(19, '2023-11-25 17:01:59', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shahe, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680 WHERE user_id=C01'),
-(20, '2023-11-25 17:02:27', 'A01', 'users', 'UPDATE', 'UPDATE users SET name=Muhammad Shaher, email=k213323@nu.edu.pk, password=naiver, phone_number=923410286680 WHERE user_id=C01'),
-(22, '2023-11-26 13:47:27', 'C01', 'commitbase', 'INSERT', 'INSERT INTO commitbase (commit_id, contributor_id, commit_path, project_id) VALUES (CommitTest,C01,https://www.forbes.com/sites/ninabambysheva/2023/02/09/ethereum-gears-up-for-next-big-upgrade-29-billion-of-ether-to-be-unlocked/,Project1)');
+INSERT INTO `transaction_log` (`log_id`, `timestamp`, `actor_id`, `operation_type`, `table_name`, `query`) VALUES
+(27, '2023-12-05 10:48:12', 'C01', 'INSERT', 'payments', 'INSERT INTO payments (sender_user_id, receiver_user_id, amount) VALUES (C01, P25, 2000);'),
+(28, '2023-12-05 10:50:16', 'P25', 'INSERT', 'payments', 'INSERT INTO payments (sender_user_id, receiver_user_id, amount) VALUES (P25, C01, 12000);'),
+(29, '2023-12-05 11:29:32', 'P81', 'INSERT', 'users', 'INSERT INTO users (user_id, name, email, password, phone_number, privilege) VALUES (P81, Jahangir, jahangir@jahangir.com, imbatman12340, 03362175634, Publisher);'),
+(30, '2023-12-05 11:46:28', 'A01', 'INSERT', 'payments', 'INSERT INTO payments (sender_user_id, receiver_user_id, amount) VALUES (A01, P25, 499);');
 
 -- --------------------------------------------------------
 
@@ -159,7 +186,7 @@ CREATE TABLE `users` (
   `phone_number` varchar(12) DEFAULT NULL,
   `privilege` varchar(11) NOT NULL,
   `status` varchar(10) NOT NULL DEFAULT 'Active'
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -167,12 +194,12 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `phone_number`, `privilege`, `status`) VALUES
 ('A01', 'Muhammad Ahmed', 'k213161@nu.edu.pk', 'manutd:*', '923052976751', 'admin', 'Active'),
-('C01', 'Muhammad Shaher', 'k213323@nu.edu.pk', 'naiver', '923410286680', 'contributor', 'Active'),
+('C01', 'Muhammad ShaheER', 'k213323@nu.edu.pk', 'naiver', '923410286680', 'contributor', 'Active'),
 ('C02', 'Zain Ali', 'k214870@nu.edu.pk', 'mostnaive', '923219216325', 'contributor', 'Suspended'),
 ('C07', 'Cristiano Ronadaldo', 'k236969@nu.edu.pk', 'siuuuuuuuuu', '927765843169', 'Contributor', 'Suspended'),
 ('C08', 'Babar Ashraf', 'k213202@nu.edu.pk', 'manutd:*', '923410286689', 'Contributor', 'Suspended'),
 ('C10', 'Fahad Ahmed', 'k214926@nu.edu.pk', 'niggafahad', '923009216325', 'Contributor', 'Active'),
-('C11', 'Owais Ali', 'k213298@nu.edu.pk', 'hammas', '921234567890', 'Contributor', 'Active'),
+('C11', 'Owais Ali2', 'k213298@nu.edu.pk', 'hammas', '921234567890', 'Contributor', 'Active'),
 ('C12', 'Ammad Hasan', 'k213218@nu.edu.pk', 'hammas', '921234567899', 'Contributor', 'Active'),
 ('C13', 'Mohammad Ali', 'k213228@nu.edu.pk', 'hammas', '921234567897', 'Contributor', 'Active'),
 ('C15', 'Mohammad Asif', 'k214258@nu.edu.pk', 'hammas', '921234567891', 'Contributor', 'Active'),
@@ -194,9 +221,35 @@ INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `phone_number`, `pr
 ('P44', 'Kemra', 'kemra@gmail.com', 'sfwefwe', '332523524542', 'Publisher', 'Active'),
 ('P56', 'Haroon Khan', 'haroon@khan.com', 'wqwefewf', '2143242432', 'Publisher', 'Active'),
 ('P76', 'Kamran Ghulam', 'kamran@ghulam.com', 'fwefwef', '13241341', 'Publisher', 'Active'),
+('P81', 'Jahangir', 'jahangir@jahangir.com', 'imbatman12340', '03362175634', 'Publisher', 'Active'),
 ('P86', 'Ali', 'ali@ali.com', 'fewfwe', '342352', 'Publisher', 'Active'),
+('P87', 'Yahya', 'yahya@yahya.com', 'nucesfast', '923456753451', 'Publisher', 'Active'),
 ('P90', 'Graves', 'graves@graves.com', 'graves', '24235346534', 'Publisher', 'Active'),
 ('P99', 'Khanna', 'khanna@khanna.com', 'johnny', '2412343215', 'Publisher', 'Active');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `usersTriggerInsert` AFTER INSERT ON `users` FOR EACH ROW BEGIN
+  DECLARE sql_text LONGTEXT;
+
+  SELECT CONCAT('INSERT INTO users (user_id, name, email, password, phone_number, privilege) VALUES (', NEW.user_id, ', ', NEW.name, ', ', NEW.email, ', ', NEW.password, ', ', NEW.phone_number, ', ', NEW.privilege, ');')
+  INTO sql_text
+  FROM information_schema.processlist
+  WHERE id = CONNECTION_ID();
+
+  INSERT INTO transaction_log (actor_id, operation_type, table_name, query)
+  VALUES (NEW.user_id, 'INSERT', 'users', sql_text);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `usersTriggerUpdate` AFTER UPDATE ON `users` FOR EACH ROW BEGIN
+  CALL UpdateUser(NEW.user_id, NEW.name, NEW.email, NEW.password, NEW.phone_number);
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -214,8 +267,8 @@ CREATE TABLE `walletbase` (
 --
 
 INSERT INTO `walletbase` (`user_id`, `wallet_address`) VALUES
-('A01', '0x242c4eA92Dc29F4af6aE499dFe11FC083053EF5e'),
-('C01', '0xeF3c34A27521f15ca5D132b91c511dEb94deaE0E'),
+('A01', '0xD3CECBC2de13f1eE2f381f5cF0C7864A8108f937'),
+('C01', '0xe165c933Fb9d5aAB5c113c2B99C594FcDC4E2A42'),
 ('P25', '0x118De23b4A3d1bD029b454C9c4a2B10Ee00218C7');
 
 --
@@ -251,7 +304,7 @@ ALTER TABLE `projectbase`
 --
 ALTER TABLE `transaction_log`
   ADD PRIMARY KEY (`log_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`actor_id`);
 
 --
 -- Indexes for table `users`
@@ -277,7 +330,7 @@ ALTER TABLE `walletbase`
 -- AUTO_INCREMENT for table `transaction_log`
 --
 ALTER TABLE `transaction_log`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
@@ -307,7 +360,7 @@ ALTER TABLE `projectbase`
 -- Constraints for table `transaction_log`
 --
 ALTER TABLE `transaction_log`
-  ADD CONSTRAINT `transaction_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `transaction_log_ibfk_1` FOREIGN KEY (`actor_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `walletbase`
